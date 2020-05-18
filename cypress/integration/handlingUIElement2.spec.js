62edefab7cb8490681d9327c1c05246bba04e276
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-18 22:40:33 +1000

    log in page

--- cypress/integration/handlingUIElement2.spec.js
+++ cypress/integration/handlingUIElement2.spec.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
 
 describe('Interact with UI', ( )=> {
-    it('verify UI Element', () => {
-        
+    it('verify checkboxes and all', () => {
+        cy.visit('http://demo.automationtesting.in/') // go to the URL
+
+        cy.get('#btn2').should('be.visible').click() // click on skip sign in button
+
+        cy.title().should('eq', "Register") // verify title of the new page
     })
 })
