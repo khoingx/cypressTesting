b3001082bac7cafb330f4b3b093ef98284921ccf
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-18 10:59:48 +1000

    add new file

---
+++ cypress/integration/handlingUIElement.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('UI Element', () => {
+    it('Verify inputbox and radio button', () => {
+        cy.visit('http://newtours.demoaut.com/') //visit the page
+
+        
+    })
+})
