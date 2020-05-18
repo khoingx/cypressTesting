f3be46b0015d693afc71816a328af9fe11fbaa7e
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-18 21:54:38 +1000

    add

--- cypress/integration/handlingUIElement.spec.js
+++ cypress/integration/handlingUIElement.spec.js
@@ -4,6 +4,11 @@ describe('UI Element', () => {
     it('Verify inputbox and radio button', () => {
         cy.visit('http://newtours.demoaut.com/') //visit the page
 
-        
+        cy.url().should('include', 'newtours') // verify URL should contain a specific content
+
+        cy.get('input[name="userName"]').should('be.visible').should('be.enabled').type("mercury") //verify username input and input username
+
+        cy.get('input[name="password"]').should('be.visible').should('be.enabled').type("mercury") //verify password input and input password
+
     })
 })
