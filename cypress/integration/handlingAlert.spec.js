6d22a6be29367321835f86315eae37ae94cd0c97
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 22:37:18 +1000

    confirmation alert

--- cypress/integration/handlingAlert.spec.js
+++ cypress/integration/handlingAlert.spec.js
@@ -4,10 +4,16 @@ describe('deal with alert suite', () => {
     it('Alert', () => {
       //  cy.get('https://mail.rediff.com/cgi-bin/login.cgi')
     
-        cy.visit('http://demo.automationtesting.in/Alerts.html')
-        cy.get('.btn-danger').click()
-        cy.on('window:alert',(str) => {
-            expect(str).to.equal('I am an alert box!')
+        // cy.visit('http://demo.automationtesting.in/Alerts.html')
+        // cy.get('.btn-danger').click()
+        // cy.on('window:alert',(str) => {
+        //     expect(str).to.equal('I am an alert box!')
+        // })
+
+        cy.visit('http://testautomationpractice.blogspot.com/')
+        cy.get('button[onclick="myFunction()"]').click()
+        cy.on('window:confirm', (str) => {
+            expect(str).to.equal('Press a button!')
         })
     })
 })
