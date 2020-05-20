43ca0cf9b8b1acb986aa49d169a81a22412f61d4
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 23:02:23 +1000

    more code

--- cypress/integration/pageNavigation.spec.js
+++ cypress/integration/pageNavigation.spec.js
@@ -3,6 +3,13 @@
 describe('Page Navigation suite', () => {
     it('navigate between pages', () => {
         cy.visit('https://demo.nopcommerce.com/')
-        
+        cy.title().should('eq', "nopCommerce demo store") //grab a title of the first page
+
+        cy.get('.ico-register').click()
+        cy.title().should('eq', 'nopCommerce demo store. Register') //grab a title of the second page
+
+        cy.go('back')
+        cy.title().should('eq', "nopCommerce demo store") //grab a title of the page
+
     })
 })
