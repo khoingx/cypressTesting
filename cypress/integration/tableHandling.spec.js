79f381c8cd281efd59e6cfa0f94aef8590954ed5
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 23:11:07 +1000

    table handling

---
+++ cypress/integration/tableHandling.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Table Handling Test Suite', ()=> {
+    it('deal with table and data', () => {
+        cy.visit('http://testautomationpractice.blogspot.com/')
+        
+    })
+})
