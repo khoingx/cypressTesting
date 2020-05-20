5d3d9151ce22046276a1fd4d798073886c5be1bb
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 22:58:16 +1000

    page navigation

---
+++ cypress/integration/pageNavigation.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Page Navigation suite', () => {
+    it('navigate between pages', () => {
+        cy.visit('https://demo.nopcommerce.com/')
+        
+    })
+})
