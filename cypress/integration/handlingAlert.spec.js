e8808faa39e9b86090694bfb4f31ab6d6e4f3059
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 22:09:05 +1000

    add new test suite

---
+++ cypress/integration/handlingAlert.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('deal with alert suite', () => {
+    it('Alert', () => {
+        cy.get('https://mail.rediff.com/cgi-bin/login.cgi')
+        
+    })
+})
