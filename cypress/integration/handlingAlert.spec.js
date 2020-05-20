6b16d347245280a39c703ddee85ef499155e60f0
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-20 22:26:51 +1000

    OK alert

--- cypress/integration/handlingAlert.spec.js
+++ cypress/integration/handlingAlert.spec.js
@@ -2,7 +2,12 @@
 
 describe('deal with alert suite', () => {
     it('Alert', () => {
-        cy.get('https://mail.rediff.com/cgi-bin/login.cgi')
-        
+      //  cy.get('https://mail.rediff.com/cgi-bin/login.cgi')
+    
+        cy.visit('http://demo.automationtesting.in/Alerts.html')
+        cy.get('.btn-danger').click()
+        cy.on('window:alert',(str) => {
+            expect(str).to.equal('I am an alert box!')
+        })
     })
 })
