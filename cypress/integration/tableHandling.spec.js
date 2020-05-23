d327a5a4f25d2dcf6bb5f049970952c4dc11d16b
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-23 21:24:57 +1000

    more data table validation

--- cypress/integration/tableHandling.spec.js
+++ cypress/integration/tableHandling.spec.js
@@ -10,6 +10,16 @@ describe('Table Handling Test Suite', ()=> {
         //Check if the value presence in specific row and column
         cy.get('table[name="BookTable"] > tbody > tr:nth-child(2) > td:nth-child(3)').contains('Selenium').should('be.visible')
 
+        //Grab a book name of which author is "amod"
+        cy.get('table[name="BookTable"] > tbody > tr td:nth-child(2)').each(($e,index,$list) => {
 
+            var text = $e.text();
+            if(text.includes("Amod")) {
+                cy.get('table[name="BookTable"] > tbody > tr td:nth-child(1)').eq(index).then((bname) => {
+                    const bookName = bname.text()
+                    expect(bookName).to.equal('Master In Java')
+                })
+            }
+        })
     })
 })
