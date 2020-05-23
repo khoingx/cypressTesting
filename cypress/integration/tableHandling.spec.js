51c318a6b2f8f34a453d52e2a5f18a46d842c5fe
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-23 20:53:18 +1000

    work w table

--- cypress/integration/tableHandling.spec.js
+++ cypress/integration/tableHandling.spec.js
@@ -3,6 +3,13 @@
 describe('Table Handling Test Suite', ()=> {
     it('deal with table and data', () => {
         cy.visit('http://testautomationpractice.blogspot.com/')
-        
+
+        //Check if the value presence somewhere in the table
+        cy.get('table[name="BookTable"]').contains('td','Learn Selenium').should('be.visible')
+
+        //Check if the value presence in specific row and column
+        cy.get('table[name="BookTable"] > tbody > tr:nth-child(2) > td:nth-child(3)').contains('Selenium').should('be.visible')
+
+
     })
 })
