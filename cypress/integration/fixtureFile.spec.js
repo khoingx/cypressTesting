4e3138f825cbe3fc20b8e08608bf45f7cf98f5b4
khoingx <55979401+khoingx@users.noreply.github.com>
2020-05-25 23:10:54 +1000

    run test with fixtures file

--- cypress/integration/fixtureFile.spec.js
+++ cypress/integration/fixtureFile.spec.js
@@ -1,17 +1,22 @@
 /// <reference types="cypress" />
 
-describe('My Test Suite', () => {
+describe('My Test Suite', function() {
 
-    before(() => {
-        cy.fixture('example').then((data) => {
-            this.data = data
-        })
-    })
+    // before(function() {
+    //     cy.fixture('example').then(function(example) {
+    //         this.example = example
+    //     })
+    // })
+
     it('fixture demo test', () => {
-        cy.visit('https://admin-demo.nopcommerce.com/login')
-        cy.get('input[name="Email"]').type(this.data.email)
-        cy.get('input[name="Password"]').type(this.data.password)
-        cy.get('input[type="submit"]').click()
+        cy.fixture('example').then(function(data) {
+            this.data = data
+            cy.visit('https://admin-demo.nopcommerce.com/login')
+            cy.get('input[name="Email"]').clear().type(this.data.email)
+            cy.get('input[name="Password"]').clear().type(this.data.password)
+            cy.get('input[type="submit"]').click()
+        })
         
+
     })
 })
